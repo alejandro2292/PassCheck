972ff0545df65ecbbc890ee0b0eab319b9db49cc
alejandro2292 <trashmailjunk0085@gmail.com>
2025-09-15 22:57:22 +0200

    Add main block to test password rules

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello world")
+
+#Functions to check the presence of different character types
+
+#Check for uppercase letters
+def has_upper(password: str) -> bool: 
+    return any(c.isupper() for c in password)
+
+#Check for lowercase letters
+def has_lower(password: str) -> bool:
+    return any(c.islower() for c in password)
+
+#Check for digits
+def has_digit(password: str) -> bool:
+    return any(c.isdigit() for c in password)
+
+#Check for special characters
+def has_special(password: str) -> bool:
+    special_char = "!@#$%^&*()-_=+[]{};:,<.>/?\\|`~"
+    return any(c in special_char for c in password)
+
+
+
+if __name__ == "__main__":  
+
+    password = input('Enter your password : ') #Password recovery
+
+    #Print password analysis for the user
+    print("\nPassword analysis : ")
+    print(f"Contains uppercase letter ? {has_upper(password)}")
+    print(f"Contains lower letter ? {has_lower(password)}")
+    print(f"Contains digit ? {has_digit(password)}")
+    print(f"Contains special character ? {has_special(password)}")
+
+
+
